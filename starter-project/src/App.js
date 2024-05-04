2ff36b8f9592319274df10c0ec25dbe12bcab6f3
Souparna Bose <souparnabose99@gmail.com>
2024-05-04 14:31:46 +0530

    Update project details

--- starter-project/src/App.js
+++ starter-project/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import  { useState } from "react"
 
 function App() {
 
-  let count = 0;
+  const [count, setCount] = useState(0)
+  console.log(useState(0))
+
+  function subCounter(){
+    setCount(count - 1)
+  }
+
+  function resetCounter(){
+    setCount(0)
+  }
+
   return (
     <div className="App">
       <div className="box">
         <p>{count}</p>
+        <button onClick={() => setCount(count => count + 3)} className="add">ADD</button>
+        <button onClick={subCounter} className="sub">SUB</button>
+        <button onClick={resetCounter} className="reset">RESET</button>
       </div>
     </div>
   );
