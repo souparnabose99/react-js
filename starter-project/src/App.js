6756193fb1f777ac0649736c320205d02b0b748c
Souparna Bose <souparnabose99@gmail.com>
2024-05-04 14:11:37 +0530

    Update project details

--- starter-project/src/App.js
+++ starter-project/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
 
 function App() {
+
+  let count = 0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <p>{count}</p>
+      </div>
     </div>
   );
 }
