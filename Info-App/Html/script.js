3f2cd88f6445cf830055979d42ea3b3fc38b6b55
Souparna Bose <souparnabose99@gmail.com>
2024-05-18 21:51:14 +0530

    Update project files

--- Info-App/Html/script.js
+++ Info-App/Html/script.js
@@ -1,12 +1,12 @@
 
-const btn = document.querySelector("btn-open");
+const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form")
 
 btn.addEventListener("click", function (){
     if (form.classList.contains("hidden")){
-            form.classList.remove("hidden")
-            btn.textContent("Close")
+        form.classList.remove("hidden")
+        btn.textContent("12")
     } else {
-        form.classList.add("hidden")
+        form.classList.add("hidden");
     }
 })
